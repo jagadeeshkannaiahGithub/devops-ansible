cde0fe192ac17e485fa1a490814ef1fe9498fbfb
jagadeeshkannaiahGithub <86254225+jagadeeshkannaiahGithub@users.noreply.github.com>
2021-06-26 16:23:09 +0000

    Update server.tf

--- server.tf
+++ server.tf
@@ -29,7 +29,7 @@ resource "aws_internet_gateway" "Dev-igw" {
 }
 
 resource "aws_default_route_table" "example" {
-  default_route_table_id = aws_vpc.aws_vpc.Devops_vpc.id.default_route_table_id
+  default_route_table_id = aws_vpc.Devops_vpc.default_route_table_id
 
   route {
     cidr_block = "172.16.0.0/16"
