2e1e808f79669020018720185f63e593e63b71f9
jagadeeshkannaiahGithub <86254225+jagadeeshkannaiahGithub@users.noreply.github.com>
2021-06-26 18:22:56 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -18,15 +18,13 @@ resource "aws_instance" "myawsserver" {
     Name = "Mcms-ec2-instance"
     env = "test"
   }
-
-
-  provisioner "local-exec" {
-   command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} >> /root/inv"
-   depends_on = [time_sleep.wait_60_seconds]
-  }
 }
   resource "time_sleep" "wait_60_seconds" {
   depends_on = [aws_instance.myawsserver]
 
   create_duration = "60s"
 }
+  provisioner "local-exec" {
+   command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} >> /root/inv"
+   depends_on = [time_sleep.wait_60_seconds]
+  }
