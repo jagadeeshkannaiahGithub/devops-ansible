2f13f1849ee4825caf75352e5ca0c37efdbcdd9c
jagadeeshkannaiahGithub <86254225+jagadeeshkannaiahGithub@users.noreply.github.com>
2021-06-26 15:54:51 +0000

    Update server.tf

--- server.tf
+++ server.tf
@@ -33,9 +33,9 @@ resource "aws_default_security_group" "default" {
 
   ingress {
     protocol  = -1
-    self      = true
     from_port = 0
     to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
