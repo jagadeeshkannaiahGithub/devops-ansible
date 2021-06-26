0804200be214a19f9e86acb68d3ba38bc67b4528
jagadeeshkannaiahGithub <86254225+jagadeeshkannaiahGithub@users.noreply.github.com>
2021-06-26 18:28:03 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -19,14 +19,4 @@ resource "aws_instance" "myawsserver" {
     env = "test"
   }
 }
-  resource "time_sleep" "wait_60_seconds" {
-  depends_on = [aws_instance.myawsserver]
 
-  create_duration = "60s"
-}
-resource "aws_instance" "myawsserver" {
-  provisioner "local-exec" {
-   command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} >> /root/inv"
-   depends_on = [time_sleep.wait_60_seconds]
-  }
-}
