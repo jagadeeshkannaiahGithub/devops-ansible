4fbe4c21bc08b553a01d97bd50b8d4f4e7f299f2
jagadeeshkannaiahGithub <86254225+jagadeeshkannaiahGithub@users.noreply.github.com>
2021-06-26 15:47:14 +0000

    Update server.tf

--- server.tf
+++ server.tf
@@ -28,30 +28,21 @@ resource "aws_internet_gateway" "Dev-igw" {
   }
 }
 
-resource "aws_security_group" "default" {
-  name        = "default"
-  description = "Allow all inbound traffic"
-  vpc_id      = aws_vpc.Devops_vpc.id
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.mainvpc.id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
