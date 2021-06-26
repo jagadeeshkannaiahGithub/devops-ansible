b9a05d9fffde4a33ea0909acab5dd3b8dc14d71c
jagadeeshkannaiahGithub <86254225+jagadeeshkannaiahGithub@users.noreply.github.com>
2021-06-26 18:32:09 +0000

    Update enable_ssh.tf

--- enable_ssh.tf
+++ enable_ssh.tf
@@ -3,9 +3,3 @@
 
   create_duration = "60s"
 }
-resource "aws_instance" "myawsserver" {
-  provisioner "remote-exec" {
-   command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} >> /root/inv"
-   depends_on = [time_sleep.wait_60_seconds]
-  }
-}
