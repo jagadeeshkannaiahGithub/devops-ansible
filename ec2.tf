f02d8c988b360d9c0cbdd4e8b1455d43a75f7fe2
jagadeeshkannaiahGithub <86254225+jagadeeshkannaiahGithub@users.noreply.github.com>
2021-06-27 08:04:50 +0000

    Update and rename ec2.txt to ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -25,11 +25,6 @@ resource "aws_instance" "myawsserver" {
   }
 }
 
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.myawsserver.id
-  instance_id = aws_instance.myawsserver.id
-}
 
   resource "aws_ebs_volume" "myawsserver" {
   availability_zone = "eu-east-2a"
