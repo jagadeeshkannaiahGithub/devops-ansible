6a1eea2af36e73acd358bc0c5f88698a2976a3cd
jagadeeshkannaiahGithub <86254225+jagadeeshkannaiahGithub@users.noreply.github.com>
2021-06-27 07:28:59 +0000

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -1,13 +1,9 @@
 resource "aws_ebs_volume" "ec2-ebs" {
  /* count             = "${var.ec2_count}" */
- /* availability_zone = "${var.availability_zone}" */
+    availability_zone = "eu-west-2a" 
  /* kms_key_id        =  "xxxx"  */
   encrypted         =   "true"
  /* size              =   "${var.vol_size_details_xvdf}" */
 /*  type              =   "${var.vol_type_details}" */
-  tags {
-    Name        =   "d-drive"
-    Owner       =   "mcms"
-    Managed_By  =   "Terraform"
-  }
+  
 }
